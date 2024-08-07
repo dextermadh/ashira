5b3597ad04fc627c6f1fc7afcb85a7771cde4d88
dextermadh <madhukaabhishek30@gmail.com>
2024-08-07 11:43:57 +0530

    Update Code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,37 +1,35 @@
 "use client"
-import React from "react";
-import { useEffect } from 'react';
-import "./brand.css"
+
+import React, { useEffect } from "react";
+import "./brand.css";
 import VelocityScroll from "@/components/VelocityScroll";
-import Lenis from 'lenis'
+import Lenis from 'lenis';
 import { Description, Description1, Intro, Section, Section1 } from "./component/about";
 import { IntroAbout } from "./Components/GradualSpacing";
 
 const Page = () => {
+    useEffect(() => {
+        const lenis = new Lenis();
 
-    useEffect( () => {
-        const lenis = new Lenis()
-    
-        function raf(time) {
-          lenis.raf(time)
-          requestAnimationFrame(raf)
+        function raf(time: number) {  // Specify the type of 'time' as 'number'
+            lenis.raf(time);
+            requestAnimationFrame(raf);
         }
-    
-        requestAnimationFrame(raf)
-      }, [])
-    return (
-    <div className="">
-    <IntroAbout />
-    <div className="content">
-    <Intro />
-    <Description />
-    <Section />
-    <Description1 />
-    <Section1 />
-    </div> 
 
-    </div>
-    
+        requestAnimationFrame(raf);
+    }, []);
+
+    return (
+        <div className="">
+            <IntroAbout />
+            <div className="content">
+                <Intro />
+                <Description />
+                <Section />
+                <Description1 />
+                <Section1 />
+            </div> 
+        </div>
     );
 };
 
